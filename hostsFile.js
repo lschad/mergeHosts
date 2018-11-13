a34f185c574889ae6521e1e8a6109f1bdb3de58f
Lucas Schad <schad.lucas@gmail.com>
2018-11-13 03:37:53 +0100

    fixed typo

--- hostsFile.js
+++ hostsFile.js
@@ -2,7 +2,7 @@ const os = require('os');
 const plat = os.platform();
 
 let hostsFile = '';
-if (plat === 'win32') hostsFile = 'c:\\windows\\system32\\drives\\etc\\hosts';
+if (plat === 'win32') hostsFile = 'c:\\windows\\system32\\drivers\\etc\\hosts';
 else if (plat === 'darwin') hostsFile = '/private/etc/hosts';
 else if (plat === 'linux') hostsFile = '/etc/hosts';
 else throw 'platform not supported.';
